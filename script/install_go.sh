c46454c4ae4f0ef5357b8bd9aea8de763e96cd46
BDIFluky <boredomdidit+github@gmail.com>
2024-10-09 20:35:22 +0200

    Update install_go.sh

--- script/install_go.sh
+++ script/install_go.sh
@@ -5,29 +5,6 @@ SGOPATH="${GOPATH:-$HOME/go}"
 SGOROOT="${GOROOT:-/opt/}"
 READ_TIMEOUT=3
 
-# Function to ask if the user wants to change the variable
-keep_or_change_env() {
-    local var_value 
-    var_value= $(eval "echo \$$1")
-    echo -e "\e[34m$1\e[0m=\e[33m'${var_value}'\e[0m"
-    
-    # Loop until the user provides a valid response
-    while true; do
-        read -t "$READ_TIMEOUT" -p "Do you want to keep this value? (y/n) [Timeout in $READ_TIMEOUT seconds]: " response
-        response="${response:-y}"  # Default to 'y' if no input within timeout
-
-        # Check for valid response
-        if [[ "$response" =~ ^[Yy]$ ]]; then
-            break
-        elif [[ "$response" =~ ^[Nn]$ ]]; then
-            read -p "Enter new value for $1: " new_value
-            eval "$1='\"$new_value\"'"
-            break
-        else
-            echo -e "\e[31mInvalid input. Please enter 'y' or 'n'.\e[0m"
-        fi
-    done
-}
 
 fetch_and_install_go() {
   local go_latest_version
