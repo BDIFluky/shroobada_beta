7877b7d59a4c8c407cce52a14604cdf7581e5fae
BDIFluky <boredomdidit+github@gmail.com>
2024-10-22 23:16:44 +0200

    Update fire_in_the_hole.sh

--- script/fire_in_the_hole.sh
+++ script/fire_in_the_hole.sh
@@ -17,10 +17,87 @@ check_privileges() {
 }
 
 # Export project envs
-export_project_envs(){
-  export $(grep -v '^#' .shroo.env | xargs)
+setup_shroober(){
+  id -u $shroober > /dev/null 2>&1 && sudo useradd -r -s /usr/sbin/nologin -d /var/lib/$shrooHPDir -m $shroober
+  nextUID=$(awk -F: '{print $2 + $3}' "/etc/subuid" | sort -n | tail -n1)
+  ! grep $shroober /etc/subuid && sudo usermod --add-subuids "$nextUID-$((nextUID + 65535))" "$shroober"
+  
+  nextGID=$(awk -F: '{print $2 + $3}' "/etc/subgid" | sort -n | tail -n1)
+  ! grep $shroober /etc/subgid && sudo usermod --add-subgids "$nextGID-$((nextGID + 65535))" "$(sudo -u $shroober bash -c "id -g -n")"
+  
+  sudo loginctl enable-linger $shroober
 }
+
+
+add_repos(){
+# Define the file paths
+  REPO_FILE="/etc/apt/sources.list.d/added_repos.list"
+  PREFERENCES_FILE="/etc/apt/preferences.d/main-priorities"
+  
+  # 1. Precheck for 'bookworm-backports' repository
+  if ! grep -q "bookworm-backports" "$REPO_FILE"; then
+      echo 'deb http://ftp.debian.org/debian bookworm-backports main contrib non-free' | sudo tee -a "$REPO_FILE"
+  fi
+  
+  # 2. Precheck for 'trixie' repository
+  if ! grep -q "trixie" "$REPO_FILE"; then
+      echo 'deb http://ftp.debian.org/debian trixie main contrib non-free' | sudo tee -a "$REPO_FILE"
+  fi
+  
+  # 3. Precheck for 'sid' repository
+  if ! grep -q "sid" "$REPO_FILE"; then
+      echo 'deb http://ftp.debian.org/debian sid main contrib non-free' | sudo tee -a "$REPO_FILE"
+  fi
+  
+  # 4. Precheck for the APT preferences before appending
+  if ! grep -q "Pin: release a=bookworm" "$PREFERENCES_FILE"; then
+      sudo tee -a "$PREFERENCES_FILE" <<EOF
+# Priority for Bookworm (Stable)
+Package: *
+Pin: release a=bookworm
+Pin-Priority: 900
+
+# Priority for Bookworm-backports
+Package: *
+Pin: release a=bookworm-backports
+Pin-Priority: 700
+
+# Priority for Trixie (Testing)
+Package: *
+Pin: release a=trixie
+Pin-Priority: 500
+
+# Priority for Sid (Unstable)
+Package: *
+Pin: release a=sid
+Pin-Priority: 400
+EOF
+  fi
+
+}
+
+
+shrooA="$(whoami)"
+shrooAPDir="$HOME/shroobada"
+shroober="chimken"
+shrooberUID="$(id -u $shroober)"
+shrooberGID="$(id -g $shroober)"
+shrooHPDir="/var/lib/$shroober"
+shrooCPDir="$shrooHPDir/shroobada"
+shrooPDir="$shrooHPDir/shroobada"
+
+shrooTraefikDir="/etc/traefik"
+shrooTraefikLogDir="/var/log/traefik"
+
+shrooAuthDir="/etc/authentik"
+shrooAuthDB="/var/lib/authdb"
+
+shrooGuacDir="/etc/guacamole"
+shrooGuacDB="/var/lib/guacdb"
 
 check_privileges()
 
-export_project_envs()
+setup_shroober()
+
+
+
